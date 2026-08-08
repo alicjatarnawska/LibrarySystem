5a65808094df37c1a9afe1df532ade35c9df35f7
Alicja Tarnawska <tarnawska.ad@gmail.com>
2026-08-08 16:46:24 +0200

    Test ReviewService in Main with try-catch

--- src/library/menu/Main.java
+++ src/library/menu/Main.java
@@ -2,11 +2,14 @@ package library.menu;
 
 import library.entities.Book;
 import library.entities.BookLoan;
+import library.entities.Review;
 import library.entities.User;
 import library.repository.BookLoanRepository;
 import library.repository.BookRepository;
+import library.repository.ReviewRepository;
 import library.repository.UserRepository;
 import library.service.BookService;
+import library.service.ReviewService;
 import library.service.UserService;
 
 import java.util.List;
@@ -19,6 +22,8 @@ public class Main {
         BookLoanRepository bookLoanRepository = new BookLoanRepository();
         BookService bookService = new BookService(bookRepository, userRepository, bookLoanRepository);
         UserService userService = new UserService(userRepository);
+        ReviewRepository reviewRepository = new ReviewRepository();
+        ReviewService reviewService = new ReviewService(reviewRepository, bookRepository, userRepository);
 
         bookService.addBook("Hobbit", "J.R.R. Tolkien", 1937, "Fantasy", "Klasyka fantasy");
         bookService.addBook("Diuna", "Frank Herbert", 1965, "Sci-Fi");
@@ -26,11 +31,17 @@ public class Main {
         LocalDate dueDate = LocalDate.now().plusDays(14);
         BookLoan loan = bookService.borrowBook(1, user.getId(), dueDate);
         bookService.returnBookLoan(loan.getId());
-
+        Review review = reviewService.addReview(1, user.getId(), 4, "Nudna książka");
+        try {
+            Review review2 = reviewService.addReview(1, user.getId(), 5, "Nudy");
+        } catch (IllegalStateException e) {
+            System.out.println("Błąd: " + e.getMessage());
+        }
 
         List<Book> books = bookService.getAllBooks();
         for (Book book : books) {
             System.out.println(book);
         }
+        System.out.println(review);
     }
 }
